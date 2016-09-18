96c8171235d32c2e3736172f5a86ea8a760aff7f
Taka <taka@Jasons-iMac.local>
2016-09-18 13:16:44 -0400

    corrected linkage with css page

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,8 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+//creating messages if cards match
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}else {alert("Sorry, try again.")}
