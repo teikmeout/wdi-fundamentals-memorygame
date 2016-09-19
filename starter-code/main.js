ba7db9dbebd19393c0d3367f6400b3d8c850db6b
Taka <taka@Jasons-iMac.local>
2016-09-19 03:26:00 -0400

    creation of cards via JS incomplete

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,36 @@ var cardThree = "queen";
 var cardFour = "king";
 
 //creating messages if cards match
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("You found a match!");
-}else {alert("Sorry, try again.")}
+}else {alert("Sorry, try again.")}*/
+
+
+//create cards using javascript
+//declaration of variable to select game board
+var board = document.getElementsById('gameboard');
+
+//function to create cards in gameboard
+var makeCards = function(){
+	//using for to make only 4 cards
+	for( var i=0; i<4; i++){
+	//declaring each div
+	var newDiv = document.createElement('div');
+	// also giving each element a class=card
+	newDiv.className = 'card';
+	//appending div to class=board
+	document.getElementsByClassName('board')[i].appendChild(newDiv);
+	}
+}
+
+
+//createBoard will create HTML contacts for each card appending HTML to div with class=board
+var createBoard = function(){
+	// for loop with 4 times iteration
+	for (var i=0; i<4; i++) {
+		// create HTML for each card
+		//code is incomplete and needs some changing
+		//append created HTML to board
+		document.getElementByClassName('board')[i].appendChild()
+	}
+}
